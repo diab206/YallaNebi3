2c083e276553f98c646eee8a0d1233c8ff4afdf6
Mohamed Diab <diab206@users.noreply.github.com>
2025-07-10 17:38:40 +0300

    welcome user screen

---
+++ lib/welcome/welcome_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:yalla_nebi3/core/app_colors.dart';
+import 'package:yalla_nebi3/views/auth/ui/login_view.dart';
+import 'package:yalla_nebi3/views/auth/ui/sign_up_view.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String routeName = 'welcome';
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.welcomebackgroundcolor,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                   Image.asset(
+                    'assets/images/welcome_screen.png',
+                    width: 350,
+                    height: 350,
+                    fit: BoxFit.contain,
+                   
+                   ),
+                
+                const SizedBox(height: 30),
+
+                // Welcome text
+                const Text(
+                  'Welcome!',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+
+                // Subtitle
+                const Text(
+                  'Sign in or create a new account',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black87,
+                  ),
+                ),
+                const SizedBox(height: 40),
+
+                // Buttons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    // Login Button
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            // ignore: deprecated_member_use
+                            backgroundColor: Colors.white.withOpacity(0.7),
+                            foregroundColor: Colors.black,
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.pushNamed(context, LoginView.routeName);
+                          },
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // Sign Up Button
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            // ignore: deprecated_member_use
+                            backgroundColor: Colors.white.withOpacity(0.7),
+                            foregroundColor: Colors.black,
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.pushNamed(context, SignUpView.routeName);
+                          },
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
