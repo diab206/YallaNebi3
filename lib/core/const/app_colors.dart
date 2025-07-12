d28eb2c3c55c580dff0a176c7732ed0c0bd79214
Mohamed Diab <diab206@users.noreply.github.com>
2025-07-12 19:28:15 +0300

    change the app colors

---
+++ lib/core/const/app_colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  static const primaryColor = Color(0xFF3669C9);
+  static const secondaryColor = Color(0xFF03DAC6);
+  static const textColor = Color(0xFF000000);
+  static const errorColor = Color(0xFFB00020);
+  static const successColor = Color(0xFF00C853);
+  static const warningColor = Color(0xFFFFA000);
+  static const backgroundcolor = Color(0xFFe5e4e1);
+ static const welcomebackgroundcolor =  Color(0xFFf7f0e2);
+ }
+
