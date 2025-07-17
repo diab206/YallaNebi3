4829f8992fe9c36e335e6309f6417433bf153bbd
Mohamed Diab <diab206@users.noreply.github.com>
2025-07-17 18:36:51 +0300

    finish favourit view ui

--- lib/views/favourit/ui/favourit_view.dart
+++ lib/views/favourit/ui/favourit_view.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:yalla_nebi3/core/components/product_list.dart';
 
 class FavouritView extends StatelessWidget {
   const FavouritView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Column(children: [
-      // Add your favourit view widgets here
-      Text('Favourit View '),
-      
-    ],);
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(child: Text('Your Favourits Products', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+          const SizedBox(height: 16),
+         ProductList()
+        ],
+      ),
+    );
   }
 }
+
+
+
+ 
