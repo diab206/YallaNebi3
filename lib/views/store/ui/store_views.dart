69b9e4a48c8a11e63c1659b18ce44cca4ea85ea6
Mohamed Diab <diab206@users.noreply.github.com>
2025-07-17 18:34:00 +0300

    finish store view ui

--- lib/views/store/ui/store_views.dart
+++ lib/views/store/ui/store_views.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:yalla_nebi3/core/components/custom_search_bar.dart';
+import 'package:yalla_nebi3/core/components/product_list.dart';
 
 class StoreView extends StatelessWidget {
-  const StoreView({super.key});
+   StoreView({super.key});
+    final searchController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
-    return  Column(children: [
-      // Add your Store view widgets here
-      Text('Store View '),
-      
-    ],);
+    return  SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(child: Text('Welcome To Our Market', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
+          CustomSearchBar(
+            controller: searchController,
+            hintText: 'Search in Market....',
+            onSearch: () {},
+          ),
+          const SizedBox(height: 16),
+         ProductList()
+        ],
+      ),
+    );
   }
 }
+
+
+
+ 
